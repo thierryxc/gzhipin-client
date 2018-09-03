2543a95922502105e5d497b4e12a1b59602ebb05
Thirerry Xu <4730890@qq.com>
2018-09-03 13:19:16 +0800

    注册登录界面数据输入

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,11 +1,47 @@
 //登陆路由组件
 import React, { Component } from 'react'
 
+import { NavBar, WingBlank, List, InputItem, WhiteSpace, Button, Radio } from 'antd-mobile';
+
+import Logo from '../../components/logo/Logo';
+
 export default class Login extends Component {
+  
+  state = {
+    username: '',
+    password: ''
+  }
+
+  login = () => {
+    console.log(this.state);
+  }
+
+  handleChange = (name, val) => {
+    this.setState({
+      [name]: val
+    });
+  }
+
+  toRsgister = () => {
+    this.props.history.replace('register');
+  }
+
   render() {
     return (
       <div>
-         Login
+        <NavBar>硅&nbsp;谷&nbsp;直&nbsp;聘</NavBar>
+        <Logo />
+        <WingBlank>
+          <List>
+            <WhiteSpace />
+            <InputItem placeholder='请输入用户名' onChange={val => {this.handleChange('username', val)}}>用户名：</InputItem>
+            <WhiteSpace />
+            <InputItem placeholder='请输入密码' onChange={val => {this.handleChange('password', val)}} type="password">密&nbsp;&nbsp;&nbsp;码：</InputItem>
+            <WhiteSpace />
+            <Button onClick={this.login} type="primary">登&nbsp;&nbsp;&nbsp;&nbsp;陆</Button>
+            <Button onClick={this.toRsgister}>还没有账户</Button>
+          </List>
+        </WingBlank> 
       </div>
     )
   }
--- src/containers/register/Register.jsx
+++ src/containers/register/Register.jsx
@@ -7,7 +7,31 @@ import Logo from '../../components/logo/Logo';
 const ListItem = List.Item;
 
 export default class Regsiter extends Component {
+  //数据
+  state = {
+    username: '',//用户名
+    password: '',//密码
+    password2: '',//确认密码
+    type: ''//用户类型  dashen/laoban
+  }
+  //注册
+  regsiter = () => {
+    console.log(this.state);
+  }
+  //处理输入数据的改变，更新对应的状态
+  handleChange = (name, val) => {
+    //更新状态
+    this.setState({
+      [name]: val//属性名是name的值，[]将字符串改为变量！
+    });
+  } 
+
+  toLogin = () => {
+    this.props.history.replace('login');
+  }
+
   render() {
+    const type = this.state.type;
     return (
       <div>
         <NavBar>硅&nbsp;谷&nbsp;直&nbsp;聘</NavBar>
@@ -15,21 +39,21 @@ export default class Regsiter extends Component {
         <WingBlank>
           <List>
             <WhiteSpace />
-            <InputItem>用户名：</InputItem>
+            <InputItem placeholder='请输入用户名' onChange={val => {this.handleChange('username', val)}}>用户名：</InputItem>
             <WhiteSpace />
-            <InputItem type="password">密&nbsp;&nbsp;&nbsp;码：</InputItem>
+            <InputItem placeholder='请输入密码' onChange={val => {this.handleChange('password', val)}} type="password">密&nbsp;&nbsp;&nbsp;码：</InputItem>
             <WhiteSpace />
-            <InputItem type="password">确认密码：</InputItem>
+            <InputItem placeholder='请再次输入密码' onChange={val => {this.handleChange('password2', val)}} type="password">确认密码：</InputItem>
             <ListItem>
               <span>用户类型：</span>
               &nbsp;&nbsp;&nbsp;
-              <Radio>大神</Radio>
+              <Radio checked={type === 'dashen'} onChange={() => this.handleChange('type', 'dashen')}>大神</Radio>
               &nbsp;&nbsp;&nbsp;
-              <Radio>老板</Radio>
+              <Radio checked={type === 'laoban'} onChange={() => this.handleChange('type', 'laoban')}>老板</Radio>
             </ListItem>
             <WhiteSpace />
-            <Button type="primary">注&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;册</Button>
-            <Button>已有账户</Button>
+            <Button type="primary" onClick={this.regsiter}>注&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;册</Button>
+            <Button onClick={this.toLogin}>已有账户</Button>
           </List>
         </WingBlank> 
       </div>
